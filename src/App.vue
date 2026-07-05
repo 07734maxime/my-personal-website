ca43a4e33002cd328f052baaadb9aee608f59775
Maxime <maxime.arbadji@icloud.com>
2026-07-05 22:31:45 +0200

    add page name

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <script setup>
 
+import { watch } from 'vue';
 import NavbarComponent from './components/NavbarComponent.vue';
+import { useRoute } from 'vue-router';
+const route = useRoute();
 
+watch(
+  () => route.fullPath,
+  () => {
+    document.title = String(route.meta?.title || route.name || 'App');
+  },
+  { immediate: true }
+);
 </script>
 
 <template>
